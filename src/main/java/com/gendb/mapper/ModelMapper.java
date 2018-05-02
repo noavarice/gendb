3c96ec0de0a323eee90d5975dea29c74ad96d8fe
noavarice <noavarice@gmail.com>
2018-05-02 20:40:13 +0300

    removed trivial mapper method

--- src/main/java/com/gendb/mapper/ModelMapper.java
+++ src/main/java/com/gendb/mapper/ModelMapper.java
@@ -1,10 +1,8 @@
 package com.gendb.mapper;
 
-import com.gendb.dto.ColumnDto;
 import com.gendb.dto.DataTypeDto;
 import com.gendb.dto.DatabaseDto;
 import com.gendb.dto.TableDto;
-import com.gendb.model.Column;
 import com.gendb.model.DataType;
 import com.gendb.model.Database;
 import com.gendb.model.Table;
@@ -16,8 +14,6 @@ public interface ModelMapper {
 
   @Mapping(target = "name", expression = "java(dto.getName().value())")
   DataType toModel(final DataTypeDto dto);
-
-  Column toModel(final ColumnDto dto);
 
   @Mapping(target = "columns", source = "column")
   Table toModel(final TableDto dto);
