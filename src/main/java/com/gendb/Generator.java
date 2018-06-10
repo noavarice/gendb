aed301e6beb6046382b85ecaf7aea9f9e6b1cfd8
noavarice <noavarice@gmail.com>
2018-06-11 00:48:30 +0300

    doc: added documentation for Generator class

--- src/main/java/com/gendb/Generator.java
+++ src/main/java/com/gendb/Generator.java
@@ -16,6 +16,7 @@ import com.gendb.model.pure.Table;
 import com.gendb.model.validating.ValidatingDatabase;
 import com.gendb.model.wrapper.ValueWrapper;
 import com.gendb.util.MapperUtils;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -46,7 +47,14 @@ import org.slf4j.LoggerFactory;
 import org.xml.sax.SAXException;
 
 /**
- * Main class for SQL generating
+ * This class provides API for using data generation.
+ *
+ * Current class is an entry point to use library functionality.
+ * Public interface represented with two methods:
+ * <ul>
+ *   <li>{@link this#createScript(InputStream, OutputStream)};</li>
+ *   <li>{@link this#createDatabase(InputStream, Properties)}.</li>
+ * </ul>
  */
 public final class Generator {
 
@@ -117,7 +125,17 @@ public final class Generator {
     this.random = provider;
   }
 
-  private static String getBatch(final InternalGenerator generator, final int count) {
+  /**
+   * Creates a string representing values for INSERT statement
+   * of particular {@code count} of rows
+   *
+   * @param generator Object which generates data
+   * @param count Count of rows to be generated
+   * @return Result of concatenation of generated rows. Values in a row are separated
+   * by commas, each row is enclosed with brackets. Example (for {@code count} of 3:
+   * "(1, 'test', 2010-01-01), (2, 'test2', 2010-01-02), (3 'test3', 2010-01-03)"
+   */
+  private static String getBatches(final InternalGenerator generator, final int count) {
     final StringJoiner rowJoiner = new StringJoiner(",", System.lineSeparator(), ";");
     for (int i = 0; i < count; ++i) {
       final StringJoiner columnJoiner = new StringJoiner(",", "(", ")");
@@ -128,12 +146,28 @@ public final class Generator {
     return rowJoiner.toString();
   }
 
+  /**
+   * Helper method that writes {@code args} to an {@code output} stream in form of strings
+   *
+   * If no {@code args} were passed, does nothing.
+   * Null object from {@code args} will be represented as "null" (default behaviour
+   * of {@link String#valueOf(Object)} method)
+   *
+   * @param output Where to write data
+   * @param args Array of object to be written to stream. Each object will be written
+   * as a result of calling {@link String#valueOf} method on it
+   * @throws ScriptGenerationException Thrown in case of problems with actual writing
+   * to {@code output}
+   */
   private static void write(final OutputStream output, final Object... args) throws ScriptGenerationException {
-    if (args == null) {
+    if (args == null || args.length == 0) {
       return;
     }
 
-    final String summary = Arrays.stream(args).map(Object::toString).reduce("", String::concat);
+    final String summary = Arrays
+        .stream(args)
+        .map(String::valueOf)
+        .reduce("", String::concat);
     try {
       output.write(summary.getBytes());
     } catch (IOException e) {
@@ -141,6 +175,16 @@ public final class Generator {
     }
   }
 
+  /**
+   * Performs SQL script creation. Internally uses {@link this#write(OutputStream, Object...)}
+   *
+   * @param db Object containing information about database to be generated
+   * and saved as SQL script
+   * @param output Where to write generated data in form of SQL queries.
+   * This might be a {@link FileOutputStream} or some other subclass of {@link OutputStream}
+   * @throws GenerationException Thrown in case of encountering problems with writing data
+   * to {@code output} stream
+   */
   private void writeToStream(final Database db, final OutputStream output) throws GenerationException {
     final int batchSize = db.getBatchSize();
     for (final Table t : db.getTables()) {
@@ -152,19 +196,28 @@ public final class Generator {
       final int fullBatchesCount = t.getRowsCount() / batchSize;
       final String insertStatement = t.getInsertStatement();
       for (int i = 0; i < fullBatchesCount; ++i) {
-        final String batch = getBatch(generator, batchSize);
+        final String batch = getBatches(generator, batchSize);
         write(output, insertStatement, batch);
       }
 
       final int lastBatchSize = t.getRowsCount() % batchSize;
       if (lastBatchSize > 0) {
-        write(output, insertStatement, getBatch(generator, lastBatchSize), System.lineSeparator());
+        write(output, insertStatement, getBatches(generator, lastBatchSize), System.lineSeparator());
       }
 
       LOGGER.info("Finish generating table '{}'", t.getName());
     }
   }
 
+  /**
+   * Analog of {@link this#write(OutputStream, Object...)} method to write
+   * several generated rows to database through {@code insert} statement
+   *
+   * @param insert Statement used to write data to database
+   * @param generator Object generating the data
+   * @param batchCount Amount of rows to be generated
+   * @throws SQLException Thrown in case of problems while using passed {@code insert} statement
+   */
   private static void write(
       final PreparedStatement insert,
       final InternalGenerator generator,
@@ -183,6 +236,16 @@ public final class Generator {
     insert.executeBatch();
   }
 
+  /**
+   * Creates database and saves it in DBMS
+   *
+   * @param dbConfig Object containing information about database to be generated
+   * @param connection Represents connection with DBMS.
+   * It is implied that database (or schema) is created (and selected in case of MYSQL)
+   * and necessary modes are set
+   * @throws SQLException Thrown in case of problems while working with {@code connection}
+   * @throws GenerationException Various internal generation problems
+   */
   private void writeToConnection(final Database dbConfig, final Connection connection)
       throws SQLException, GenerationException {
     final Statement dmlStatement = connection.createStatement();
@@ -207,6 +270,12 @@ public final class Generator {
     dmlStatement.close();
   }
 
+  /**
+   * Performs validation upon database configuration object
+   *
+   * @param dbConfig Configuration object to be validated
+   * @throws ValidationException Thrown if constraint violations are found
+   */
   private void validate(final ValidatingDatabase dbConfig) throws ValidationException {
     final Set<ConstraintViolation<ValidatingDatabase>> violations = validator.validate(dbConfig);
     if (violations.isEmpty()) {
@@ -219,6 +288,15 @@ public final class Generator {
     throw new ValidationException(joiner.toString());
   }
 
+  /**
+   * Method responsible for database generation and saving the result in form of SQL queries
+   * to passed {@code output} stream
+   *
+   * @param input Stream of XML configuration data containing database structure
+   * @param output Where to write generating database
+   * @throws GenerationException Any problems while generating and writing data are wrapped
+   * to application-specific exceptions all of which are subclassing {@link GenerationException}
+   */
   public void createScript(final InputStream input, final OutputStream output) throws GenerationException {
     final ValidatingDatabase dbConfig = getConfig(input);
     validate(dbConfig);
@@ -226,6 +304,15 @@ public final class Generator {
     writeToStream(pureConfig, output);
   }
 
+  /**
+   * Method responsible for database generation and saving the result to database
+   * through DBMS connection
+   *
+   * @param input Stream of XML configuration data containing database structure
+   * @param connProps Properties to connect to DBMS (such as URL, user, password, etc.)
+   * @throws GenerationException Any problems while generating and writing data are wrapped
+   * to application-specific exceptions all of which are subclassing {@link GenerationException}
+   */
   public void createDatabase(final InputStream input, final Properties connProps) throws GenerationException {
     final ValidatingDatabase dbConfig = getConfig(input);
     validate(dbConfig);
